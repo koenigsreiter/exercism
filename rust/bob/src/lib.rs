b2979bd59af6dae03a197c54b96655e265756f7a
Simon Königsreiter <simon.koenigsreiter@gmail.com>
2019-04-03 21:43:21 +0200

    Updates the bob exercise based on mentor feedback

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,25 +1,33 @@
 pub fn reply(message: &str) -> &str {
-    if message.trim().is_empty() {
-        return "Fine. Be that way!";
-    }
+    let trimmed_msg: &str = message.trim();
 
-    let yelling = message
-        .chars()
-        .filter(|input_char| input_char.is_alphabetic())
-        .all(char::is_uppercase);
-    let question = message.trim().ends_with('?');
-    let no_content = message
-        .trim()
-        .chars()
-        .filter(|input_char| input_char.is_alphabetic())
-        .collect::<String>()
-        .is_empty();
+    const NO_CONTENT_MSG: &str = "Fine. Be that way!";
 
-    match (yelling, question, no_content) {
-        (true, true, false) => "Calm down, I know what I'm doing!",
-        (true, false, false) => "Whoa, chill out!",
-        (_, true, _) => "Sure.",
-        (false, false, true) => "Fine. Be that way!",
-        _ => "Whatever.",
+    if trimmed_msg.is_empty() {
+        NO_CONTENT_MSG
+    } else {
+        const YELLING_QUESTION_MSG: &str = "Calm down, I know what I'm doing!";
+        const YELLING_MSG: &str = "Whoa, chill out!";
+        const QUESTION_MSG: &str = "Sure.";
+        const WHATEVER_MSG: &str = "Whatever.";
+
+        let yelling: bool = message
+            .chars()
+            .filter(|input_char| input_char.is_alphabetic())
+            .all(char::is_uppercase);
+        let question: bool = message.trim().ends_with('?');
+        let no_content: bool = trimmed_msg
+            .chars()
+            .filter(|input_char| input_char.is_alphabetic())
+            .collect::<String>()
+            .is_empty();
+
+        match (yelling, question, no_content) {
+            (true, true, false) => YELLING_QUESTION_MSG,
+            (true, false, false) => YELLING_MSG,
+            (_, true, _) => QUESTION_MSG,
+            (false, false, true) => NO_CONTENT_MSG,
+            _ => WHATEVER_MSG,
+        }
     }
 }
