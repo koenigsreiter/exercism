ab47d0cc9795ae3a0648f4736e5aaa0f1de4dac5
Simon Königsreiter <simon.koenigsreiter@gmail.com>
2019-04-10 16:43:10 +0200

    Refactors the bob exercise based on mentor feedback

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,32 +1,32 @@
 pub fn reply(message: &str) -> &str {
-    let trimmed_msg: &str = message.trim();
-
     const NO_CONTENT_MSG: &str = "Fine. Be that way!";
+    const YELLING_QUESTION_MSG: &str = "Calm down, I know what I'm doing!";
+    const YELLING_MSG: &str = "Whoa, chill out!";
+    const QUESTION_MSG: &str = "Sure.";
+    const WHATEVER_MSG: &str = "Whatever.";
+
+    let trimmed_msg: &str = message.trim();
 
     if trimmed_msg.is_empty() {
         NO_CONTENT_MSG
     } else {
-        const YELLING_QUESTION_MSG: &str = "Calm down, I know what I'm doing!";
-        const YELLING_MSG: &str = "Whoa, chill out!";
-        const QUESTION_MSG: &str = "Sure.";
-        const WHATEVER_MSG: &str = "Whatever.";
+        let yelling: bool = {
+            let filtered_string: String = trimmed_msg
+                .chars()
+                .filter(char::is_ascii_alphabetic)
+                .collect::<String>();
+            filtered_string.len() != 0
+                && trimmed_msg
+                    .chars()
+                    .filter(char::is_ascii_alphabetic)
+                    .all(char::is_uppercase)
+        };
+        let question: bool = trimmed_msg.ends_with('?');
 
-        let yelling: bool = message
-            .chars()
-            .filter(|input_char| input_char.is_alphabetic())
-            .all(char::is_uppercase);
-        let question: bool = message.trim().ends_with('?');
-        let no_content: bool = trimmed_msg
-            .chars()
-            .filter(|input_char| input_char.is_alphabetic())
-            .collect::<String>()
-            .is_empty();
-
-        match (yelling, question, no_content) {
-            (true, true, false) => YELLING_QUESTION_MSG,
-            (true, false, false) => YELLING_MSG,
-            (_, true, _) => QUESTION_MSG,
-            (false, false, true) => NO_CONTENT_MSG,
+        match (yelling, question) {
+            (true, true) => YELLING_QUESTION_MSG,
+            (true, false) => YELLING_MSG,
+            (_, true) => QUESTION_MSG,
             _ => WHATEVER_MSG,
         }
     }
